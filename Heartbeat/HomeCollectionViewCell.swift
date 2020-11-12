057e23d5a3c7e75fb0696a69e15145cf676fb412
Lexi Diep <adiep@asu.edu>
2020-11-12 01:43:55 -0700

    Recommended panel designs updated

---
+++ Heartbeat/HomeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  HomeCollectionViewCell.swift
+//  Heartbeat
+//
+//  Created by Lexi Diep on 11/12/20.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var music_image: UIImageView!
+    @IBOutlet weak var upbeat_image: UIImageView!
+    @IBOutlet weak var moderate_image: UIImageView!
+}
--- Heartbeat/HomeController.swift
+++ Heartbeat/HomeController.swift
@@ -6,61 +6,102 @@
 //
 
 import UIKit
+import SwiftUI
 
-class HomeController: UIViewController {
+class HomeController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
     @IBOutlet weak var featured: UILabel!
     @IBOutlet weak var first_recomm: UILabel!
     @IBOutlet weak var second_recomm: UILabel!
     @IBOutlet weak var third_recomm: UILabel!
     @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var featured_panel: UIImageView!
-    @IBOutlet weak var first_recomm_panel: UIImageView!
-    @IBOutlet weak var second_recomm_panel: UIImageView!
-    @IBOutlet weak var third_recomm_panel: UIImageView!
     
     
-    var slowImages = ["antidote_travis_scott_66bpm", "congratulations_mac_miller_57bpm", "perfect_ed_sheeran_63bpm", "sign_of_the_times_harry_styles_60bpm", "something_just_like_this_chainsmokers_51bpm", "xanny_billie_eilish_50bpm"]
+    // for recommended panels
+    @IBOutlet weak var featured_slide: UICollectionView!
+    @IBOutlet weak var featured_pageCtrl: UIPageControl!
+    @IBOutlet weak var upbeat_slide: UICollectionView!
+    @IBOutlet weak var upbeat_pageCtrl: UIPageControl!
+    @IBOutlet weak var slow_slide: UICollectionView!
+    @IBOutlet weak var slow_pageCtrl: UIPageControl!
+    @IBOutlet weak var moderate_slide: UICollectionView!
+    @IBOutlet weak var moderate_pageCtrl: UIPageControl!
     
-    var moderateImages = ["ariana-7-rings_70bpm", "humble_kendrick_lamar_76bpm", "lonely_justin_bieber_79bpm", "redbone_childish_gambino_81bpm", "watermelon_sugar_harry_styles_95bpm", "you-need-to-calm-down_taylor-swift_85bpm"]
+    var timer: Timer?
+    var currentIndex = 0
+    var currentIndex_upbeat = 0
     
-    var fastImages = ["bad_and_boujee_migos_129bpm", "blinding_lights_weeknd_172bpm", "dangerous-woman_ariana-grande_136bpm", "dont_start_now_dua_lipa_123bpm", "laugh_now_cry_later_drake_133bpm", "power_kanye_west_152bpm", "thunder_imagine_dragons_166bpm"]
+    // image data for each panel
+    var slowImages = [UIImage(named:"antidote_travis_scott_66bpm") ,
+                      UIImage(named:"congratulations_mac_miller_57bpm") ,
+                      UIImage(named:"perfect_ed_sheeran_63bpm") ,
+                      UIImage(named:"sign_of_the_times_harry_styles_60bpm") ,
+                      UIImage(named:"something_just_like_this_chainsmokers_51bpm") ,
+                      UIImage(named:"xanny_billie_eilish_50bpm") ]
+    
+    var moderateImages = [UIImage(named:"ariana-7-rings_70bpm") ,
+                          UIImage(named:"humble_kendrick_lamar_76bpm") ,
+                          UIImage(named:"lonely_justin_bieber_79bpm") ,
+                          UIImage(named:"redbone_childish_gambino_81bpm") ,
+                          UIImage(named:"watermelon_sugar_harry_styles_95bpm") ,
+                          UIImage(named:"you-need-to-calm-down_taylor-swift_85bpm") ]
+    
+    var fastImages = [UIImage(named:"bad_and_boujee_migos_129bpm") ,
+                      UIImage(named:"blinding_lights_weeknd_172bpm") ,
+                      UIImage(named:"dangerous-woman_ariana-grande_136bpm") ,
+                      UIImage(named:"dont_start_now_dua_lipa_123bpm") ,
+                      UIImage(named:"laugh_now_cry_later_drake_133bpm") ,
+                      UIImage(named:"power_kanye_west_152bpm") ,
+                      UIImage(named:"thunder_imagine_dragons_166bpm") ]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-                
+        
+        // featured panel attributes
         featured.layer.borderColor = UIColor.lightGray.cgColor
         featured.layer.borderWidth = 1.0
         featured.layer.cornerRadius = 5
-        featured_panel.layer.cornerRadius = 5
+        featured_slide.layer.cornerRadius = 5
+        //featured_slide.delegate = self
+        //featured_slide.dataSource = self
+        
+        // upbeat panel attributes
         first_recomm.layer.borderColor = UIColor.lightGray.cgColor
         first_recomm.layer.borderWidth = 1.0
         first_recomm.layer.cornerRadius = 5
-        first_recomm_panel.layer.cornerRadius = 5
+        upbeat_slide.layer.cornerRadius = 5
+        upbeat_slide.delegate = self
+        upbeat_slide.dataSource = self
+        upbeat_pageCtrl.numberOfPages = fastImages.count
+        
+        // slow-paced panel attributes
         second_recomm.layer.borderColor = UIColor.lightGray.cgColor
         second_recomm.layer.borderWidth = 1.0
         second_recomm.layer.cornerRadius = 5
-        second_recomm_panel.layer.cornerRadius = 5
+        slow_slide.layer.cornerRadius = 5
+        slow_slide.delegate = self
+        slow_slide.dataSource = self
+        slow_pageCtrl.numberOfPages = slowImages.count
+        
+        // moderate panel attributes
         third_recomm.layer.borderColor = UIColor.lightGray.cgColor
         third_recomm.layer.borderWidth = 1.0
         third_recomm.layer.cornerRadius = 5
-        third_recomm_panel.layer.cornerRadius = 5
+        moderate_slide.layer.cornerRadius = 5
+        moderate_slide.delegate = self
+        moderate_slide.dataSource = self
+        moderate_pageCtrl.numberOfPages = moderateImages.count
         
-        let timer1 = Timer.scheduledTimer(withTimeInterval: 3, repeats: true) {
-            timer in self.first_recomm_panel.image = UIImage(named: self.fastImages.randomElement()!)
-        }
-        let timer2 = Timer.scheduledTimer(withTimeInterval: 3, repeats: true) {
-            timer in self.second_recomm_panel.image = UIImage(named: self.slowImages.randomElement()!)
-        }
-        let timer3 = Timer.scheduledTimer(withTimeInterval: 3, repeats: true) {
-            timer in self.third_recomm_panel.image = UIImage(named: self.moderateImages.randomElement()!)
-        }
-        timer1.fire()
-        timer2.fire()
-        timer3.fire()
+        startTimer() // begins auto scroll on each recommended panel
     }
-
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -68,6 +109,34 @@ class HomeController: UIViewController {
         scrollView.contentSize = CGSize(width: view.frame.size.width, height: view.frame.size.height)
     }
 
+    
+    // functions for panels' image auto-scrolls
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 4.5, target: self, selector: #selector(moveToNextIndex), userInfo: nil, repeats: true)
+    }
+    
+    // moves auto scroll to next image in each recommended panel
+    @objc func moveToNextIndex() {
+        if currentIndex < slowImages.count - 1 {
+            currentIndex += 1
+        }
+        else {
+            currentIndex = 0
+        }
+        if currentIndex_upbeat < fastImages.count - 1 {
+            currentIndex_upbeat += 1
+        }
+        else {
+            currentIndex_upbeat = 0
+        }
+        slow_slide.scrollToItem(at: IndexPath(item: currentIndex, section: 0), at: .centeredHorizontally, animated: true)
+        slow_pageCtrl.currentPage = currentIndex
+        upbeat_slide.scrollToItem(at: IndexPath(item: currentIndex_upbeat, section: 0), at: .centeredHorizontally, animated: true)
+        upbeat_pageCtrl.currentPage = currentIndex_upbeat
+        moderate_slide.scrollToItem(at: IndexPath(item: currentIndex, section: 0), at: .centeredHorizontally, animated: true)
+        moderate_pageCtrl.currentPage = currentIndex
+    }
+    
 
     /*
     // MARK: - Navigation
@@ -78,5 +147,52 @@ class HomeController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    
+    // data source/delegates
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        var count:Int = 0
+        if collectionView == slow_slide {
+            count = slowImages.count
+        }
+        else if collectionView == upbeat_slide {
+            count = fastImages.count
+        }
+        else if collectionView == moderate_slide {
+            count = moderateImages.count
+        }
+        return count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // slow-paced cells
+        if collectionView == self.slow_slide {
+            let slow_paced_cell = collectionView.dequeueReusableCell(withReuseIdentifier: "slow_cell", for: indexPath) as! HomeCollectionViewCell
+            slow_paced_cell.music_image.image = slowImages[indexPath.row]
+            return slow_paced_cell
+        }
+        // upbeat cells
+        else if collectionView == self.upbeat_slide {
+            let fast_cell = collectionView.dequeueReusableCell(withReuseIdentifier: "upbeat_cell", for: indexPath) as! HomeCollectionViewCell
+            fast_cell.upbeat_image.image = fastImages[indexPath.row]
+            return fast_cell
+        }
+        // moderate cells
+        else if collectionView == self.moderate_slide {
+            let mod_cell = collectionView.dequeueReusableCell(withReuseIdentifier: "moderate_cell", for: indexPath) as! HomeCollectionViewCell
+            mod_cell.moderate_image.image = moderateImages[indexPath.row]
+            return mod_cell
+        }
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 }
+
